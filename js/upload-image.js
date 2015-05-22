3885fbfd14502d27a82a7f1ad4b3d095eb29bb92
amirk27 <amirk27@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-22 15:26:59 +0000

    release version 1.6.9
    git-svn-id: https://plugins.svn.wordpress.org/jazz-popups/tags/1.6.9@1165811 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/upload-image.js
@@ -0,0 +1,248 @@
+jQuery(document).ready(function($){
+ 
+ 
+    var custom_uploader;
+ 
+ 
+    $('#upload_image_button').click(function(e) {
+ 
+        e.preventDefault();
+ 
+        //If the uploader object has already been created, reopen the dialog
+        if (custom_uploader) {
+            custom_uploader.open();
+            return;
+        }
+ 
+        //Extend the wp.media object
+        custom_uploader = wp.media.frames.file_frame = wp.media({
+            title: 'Choose Image',
+            button: {
+                text: 'Choose Image'
+            },
+            multiple: false
+        });
+ 
+        //When a file is selected, grab the URL and set it as the text field's value
+        custom_uploader.on('select', function() {
+            attachment = custom_uploader.state().get('selection').first().toJSON();
+            $('#upload_image').val(attachment.url);
+            $('#myimage').attr({src:attachment.url});
+            $('#img-msg').css("display", "block");
+        });
+ 
+        //Open the uploader dialog
+        custom_uploader.open();
+ 
+    });
+ 
+ 
+});
+//----------------------------------------------------
+jQuery(document).ready(function ($) {
+    var current_url = window.location;
+    var loc = window.location.href,
+            index = loc.indexOf('#');
+
+    if (index > 0) {
+        current_url = loc.substring(0, index);
+    }
+    var magic_url = current_url + '&jazzpopup_magic_data=1';
+    console.log(magic_url);
+    $('#magicsuggest').magicSuggest({
+        data: magic_url ,
+        ajaxConfig: {
+            xhrFields: {
+                withCredentials: true,
+            }
+        }
+    });
+//    jQuery( document ).tooltip({
+//        content: function() {
+//        var element = $( this );
+//            if ( element.is( "[help]" ) ) {
+//                return 'The Minimum width is 280px & Max is 500px';
+//            }
+//            if ( element.is( "[help1]" ) ) {
+//                return 'The Minimum height of the Like Box is 130px';
+//            }
+//        }
+//        
+//    });
+});
+
+
+jQuery(document).ready(function ($) {
+    
+    $('#magicsuggest').magicSuggest({
+        // [...] // configuration options
+    });
+});
+//----------------------------------------------------
+function wpjazzpopup_switchonoff(val) {
+    var path = jQuery(val).attr("src");
+    var file = path.split('/').pop();
+    var file2 = path.split(file);
+    console.log(file2[0]);
+    var on = '';
+    var off = '';
+    if (file == 'on.png') {
+        on = true;
+    } else {
+        off = true;
+    }
+    if (off)
+    {
+        jQuery.post('', {'wpjazzpopup_switchonoff': 1}, function (e) {
+            if (e == 'error') {
+                error('error');
+            } else {
+                jQuery('#jazz_circ').css("background", "#0f0");
+                jQuery(val).attr("src", file2[0] + 'on.png');
+            }
+        });
+    }
+    if (on) {
+        jQuery.post('', {'wpjazzpopup_switchonoff': 0}, function (e) {
+            if (e == 'error') {
+                error('error');
+            } else {
+                jQuery('#jazz_circ').css("background", "#f00");
+                jQuery(val).attr("src", file2[0] + 'off.png');
+            }
+        });
+    }
+    //alert(val);
+}
+//----------------------------------------------------
+
+//        jQuery(document).ready(function () {
+//            jQuery("input[type=checkbox]").switchButton({
+//                width: 80,
+//                height: 30,
+//                on_label: 'ON',
+//                off_label: 'OFF',
+//                button_width: 40
+//            });
+//        });
+        function get_tinymce_content() {
+            if (jQuery("#wp-content-wrap").hasClass("tmce-active")) {
+                return tinyMCE.activeEditor.getContent();
+            } else {
+                return jQuery('#content').val();
+            }
+        }
+        jQuery(document).ready(function () {
+            jQuery('button[data-reveal-id]').live('click', function (e) {
+
+                var remove = '';
+                var animation = '';
+                var position = '';
+                var content = '';
+                var tmp_image = jQuery('#upload_image').val();
+                var tmp_video = jQuery('#textarea_data').val();
+                var tmp_remove = jQuery('#wp-remove').val()
+                var tmp_background = jQuery('#wp-background').val();
+                var tmp_animation = jQuery('#wp-animation').val();
+                var tmp_width = jQuery('#wp-width').val();
+                var position = jQuery('#wp-position').val();
+
+                if (jQuery("#text").is(':checked')) {
+    //                    var iframe = document.getElementById('content_ifr');
+    //                    console.log(iframe);
+    //                    var innerDoc = iframe.contentDocument || iframe.contentWindow.document;
+    //                    var con = innerDoc.getElementById('tinymce').innerHTML;
+                    // console.log(get_tinymce_content());
+                    content = get_tinymce_content();
+                }
+                if (jQuery("#image").is(':checked')) {
+                    content = tmp_image;
+                }
+                if (jQuery("#video").is(':checked')) {
+                    content = tmp_video;
+                    tmp_background = 'none';
+                }
+                if (jQuery("#all").is(':checked')) {
+                    content = get_tinymce_content();
+                }
+                jQuery('#test-popup').html(content);
+                if (tmp_remove == 1)
+                {
+                    remove = false;
+                } else {
+                    remove = true;
+                }
+                if (tmp_animation == 1) {
+                    animation = 'mfp-zoom-in';
+                }
+                if (tmp_animation == 2) {
+                    animation = 'mfp-newspaper';
+                }
+                if (tmp_animation == 3) {
+                    animation = 'mfp-move-horizontal';
+                }
+                if (tmp_animation == 4) {
+                    animation = 'mfp-move-from-top';
+                }
+                if (tmp_animation == 5) {
+                    animation = 'mfp-3d-unfold';
+                }
+                if (tmp_animation == 6) {
+                    animation = 'mfp-zoom-out';
+                }
+
+                jQuery('#test-popup').css('background', tmp_background);
+                jQuery('#test-popup').css('max-width', tmp_width+'px');
+                console.log(content);
+                if (content == null || content == '' || content == '<p><br data-mce-bogus="1"></p>' || content == 'http://') {
+                    alert('There is no content to display');
+                    return;
+                }
+                if (jQuery("#image").is(':checked')) {
+                    console.log(content);
+                    jQuery.jazzPopup.open({
+                        items: {src: content},
+                        type: "image",
+                        removalDelay: 500,
+                        closeOnBgClick: remove,
+                        callbacks: {
+                            beforeOpen: function () {
+                                this.st.image.markup = this.st.image.markup.replace("mfp-figure", "mfp-figure mfp-with-anim");
+                                this.st.mainClass = animation;
+                            }}
+                    });
+                } else {
+                    jQuery.jazzPopup.open({
+                        items: {src: "#test-popup"},
+                        type: "inline",
+                        removalDelay: 500,
+                        closeOnBgClick: remove,
+                        callbacks: {
+                            beforeOpen: function () {
+                                this.st.image.markup = this.st.image.markup.replace("mfp-figure", "mfp-figure mfp-with-anim");
+                                this.st.mainClass = animation;
+                            }}
+                    });
+                }
+                jQuery('.mfp-img').css('max-width', tmp_width+'px');
+            });
+			
+        })
+        jQuery(document).ready(function () {
+            jQuery('.btn-file :file').on('fileselect', function (event, numFiles, label) {
+
+                var input = jQuery(this).parents('.input-group').find(':text'),
+                        log = numFiles > 1 ? numFiles + ' files selected' : label;
+
+                if (input.length) {
+                    input.val(log);
+                } else {
+                    if (log)
+                        alert(log);
+                }
+
+            });
+        });
+
+
+ 
---
+++ readme.txt
@@ -0,0 +1,113 @@
+=== Plugin Name ===
+Contributors: amirk27
+Donate link: https://www.2checkout.com/checkout/purchase?sid=102444448&quantity=1&product_id=1
+Tags: Popups, Pop-ups, welcome message, special message, custom popups, image box, video message, alertbox, alert, offer, offer, announcement, announcement box, alert box, special message, homepage box, help message, guide box, 
+Requires at least: 3.5
+Tested up to: 4.2
+Stable tag: 1.6.9
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Jazz Popups allow you to add special announcement, message or offers in form of text, image and video.
+
+== Description ==
+
+Jazz Popups allow you to add special announcements, messages, tips, offers on your homepage or every page of your wordpress website. You can add text, image or video.
+
+= Features =
+
+* Fully customizable content(Text, Video, Image);
+* Seven different animation styles;
+* Responsive layout;
+* Ability to display popup for both logged in or non logged in users;
+* Works with any WordPress theme;
+* ON/OFF Indication
+
+> <strong> Docs & Support </strong> 
+
+> You can find [docs](http://bit.ly/1Ico5dp), [FAQ](https://wordpress.org/plugins/jazz-popups/faq/) and more detailed information about WP Like Button on [CRUDLab.com](http://crudlab.com/). If you were unable to find the answer to your question on the [FAQ](https://wordpress.org/plugins/jazz-popups/faq/) or in any of the documentation, please submit ticket [[here](https://crudlab.com/submit-ticket/)].
+
+> <strong> Recommended Plugins </strong>
+
+> The following are other recommended plugins by the author of WP Like Button.
+
+> * [CRUDLab Facebook Like Box](https://wordpress.org/plugins/crudlab-facebook-like-box/) - CRUDLab Facebook Like Box allows you to add Facebook like box to your wordpress blog. 
+> * [WP Tweet Plus](https://wordpress.org/plugins/wp-tweet-plus/) - WP Tweet Plus allows you to add tweet button on your wordpress blog. You can add tweet Button homepage, specific pages and posts.
+> * [CRUDLab Google Plus Button](https://wordpress.org/plugins/crudlab-google-plus/) - CRUDLab Google Plus button let visitors recommend your content on Google Search and share it on Google+.
+> * [WP Like Button](https://wordpress.org/plugins/wp-like-button/) - WP Like button allows you to add Facebook like button on your wordpress blog.
+
+== Installation ==
+
+
+1. Upload jazz-popups folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+
+== Frequently Asked Questions ==
+
+= I added image or text or video but it doesn't display? =
+
+Please make sure that you click on update button after adding your content. Also, please make sure pop up is on. 
+
+= What does green and red circle mean? =
+
+Green means popup is enabled and red means pop up is disabled. 
+
+= How can I change popup color? =
+
+Click on Appearance on the right side, it would show color, please select color and don't forget to press update button. 
+
+= Do I need to press update button to see Preview? = 
+No, you can see changes in preview without clicking on update button. 
+
+
+= How do I set video or image width? =
+
+Please enter desired width in width field. It will be applied on your video or image. Please note that embed video width won't work and wil be overridden by our width input. 
+
+= Why doesn't Jazz Popup has height input? =
+To maintain image or video quality jazz popup applies height automatically using aspect ratio formula. All you need to do is to enter desired width and height will automatically be calculated. 
+
+
+
+== Screenshots ==
+
+1. Popup Example1
+2. Popup Example2
+3. Popup settings
+
+
+== Changelog ==
+
+= 1.6.9 =
+* Fixed some bugs
+
+= 1.6.8 =
+* Fixed "embed video code" issue.
+* Fixed preview issue
+
+= 1.6.6 = 
+* Improved "where to display" option. 
+* Added Switch for ON/OFF popup.
+* Added Support and FAQ links
+* Fixed few bugs.
+
+= 1.4.4 =
+* Fixed "WSIWYG editor changes not being saved"
+
+= 1.4.3 =
+* Added popup closing time. Now you can specify how long pop should remain open
+* Improved popup styling along with cross icon.
+* Made it compatible with older Wordpress versions (Till 3.5)
+
+
+= 1.4.0 =
+* Added ability to control pop up width. 
+* Added ability for the users to exclude specific pages and posts.
+* Added ability to display pop up on custom pages. 
+* Fixed bugs. 
+
+= 1.0 =
+* Rolled out plug-in (01-05-2015)
+
+
